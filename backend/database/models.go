4cf716b40de88eaff03902e85eca73c19e1563d4
Fredrik Vedvik <fredrik@vedvik.tech>
2023-02-10 07:13:08 +0100

    initial sanity things

---
+++ backend/database/models.go
@@ -0,0 +1,21 @@
+package database
+
+import "github.com/google/uuid"
+
+type Reference struct {
+	Ref uuid.UUID `json:"_ref"`
+}
+
+type LocaleString []struct {
+}
+
+type Song struct {
+	Collections []struct {
+		Collection Reference
+		Number     int
+	}
+	Copyrights []struct {
+		Type      string
+		Copyright Reference
+	}
+}
